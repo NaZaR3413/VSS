cb1dfa50082d68df484614b0e71a2305a63f5e61
Nilay Patel <nilayno1@gmail.com>
2024-11-14 01:31:04 -0700

    feat(livestream) - Creation of livestream API related classes

---
+++ web_backend/src/web_backend.Application/Livestreams/LivestreamAppService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace web_backend.Livestreams
+{
+    public class LivestreamAppService : ApplicationService, ILivestreamAppService
+    {
+        public LivestreamAppService() 
+        { 
+
+        }
+    }
+}
---
+++ web_backend/src/web_backend.EntityFrameworkCore/Livestreams/LivestreamRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+using web_backend.EntityFrameworkCore;
+
+namespace web_backend.Livestreams
+{
+    public class LivestreamRepository : EfCoreRepository<web_backendDbContext, Livestream, Guid>, ILivestreamRepository
+    {
+        public LivestreamRepository(IDbContextProvider<web_backendDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+        }
+    }
+}
