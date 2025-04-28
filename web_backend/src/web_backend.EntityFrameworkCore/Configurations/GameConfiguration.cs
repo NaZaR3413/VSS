ff39db81b582ce40248c0df147a8985a44d21b9f
Nilay Patel <nilayno1@gmail.com>
2025-04-27 18:12:51 -0700

    feat(games) - game table/classes/migrations made and ran.

---
+++ web_backend/src/web_backend.EntityFrameworkCore/Configurations/GameConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.EntityFrameworkCore.Modeling;
+using web_backend.Games;
+
+namespace web_backend.Configurations
+{
+    public class GameConfiguration : IEntityTypeConfiguration<Game>
+    {
+        public void Configure(EntityTypeBuilder<Game> builder)
+        {
+            builder.ToTable(nameof(Game), web_backendConsts.DbSchema);
+            builder.ConfigureByConvention();
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.GameUrl).IsRequired().HasMaxLength(256);
+            builder.Property(c => c.HomeTeam).IsRequired().HasMaxLength(128);
+            builder.Property(c => c.AwayTeam).IsRequired().HasMaxLength(128);
+            builder.Property(c => c.HomeScore).IsRequired();
+            builder.Property(c => c.AwayScore).IsRequired();
+            builder.Property(c => c.Broadcasters).HasMaxLength(256);
+            builder.Property(c => c.Description).HasMaxLength(512);
+            builder.Property(c => c.EventDate).IsRequired();
+            builder.Property(c => c.EventType).IsRequired().HasConversion<int>();
+
+        }
+    }
+}
