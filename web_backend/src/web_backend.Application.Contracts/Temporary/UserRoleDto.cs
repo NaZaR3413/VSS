7d9d7419b86110485ca1523ce13709ce3d24c465
Nilay Patel <nilayno1@gmail.com>
2025-04-30 23:49:04 -0700

    bug(user role check) - temporary bandaid to allow the app teams to check a user's role until the auth issues are figured out

---
+++ web_backend/src/web_backend.Application.Contracts/Temporary/UserRoleDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web_backend.Temporary
+{
+    public class UserRoleDto
+    {
+        public string Username { get; set; }
+        public string Role { get; set; }
+    }
+}
