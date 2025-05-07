9a929224d21f8f8c73d286d552900f0a25634cd8
Evangelos Leros <lerosevan@gmail.com>
2025-05-06 19:10:55 -0700

    Add PlaybackUrl property to Game entity
    Introduced a new required property `PlaybackUrl` to the `Game` entity in the database context, with a column type of `nvarchar(max)`. Updated the model snapshot and migration file to reflect this change, including the addition of the `PlaybackUrl` column to the `Game` table with a default value of an empty string. This allows for the storage of playback URLs for each game record.

---
+++ web_backend/src/web_backend.EntityFrameworkCore/Migrations/20250507020802_AddPlaybackUrlToGameTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace web_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlaybackUrlToGameTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PlaybackUrl",
+                table: "Game",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlaybackUrl",
+                table: "Game");
+        }
+    }
+}
