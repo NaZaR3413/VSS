a48c3a54afa9ac11deea8dec7697f15d49241139
Evangelos Leros <lerosevan@gmail.com>
2025-05-08 20:21:51 -0700

    Completion of games pages

--- web_backend/src/web_backend.Application/Games/GameAppService.cs
+++ web_backend/src/web_backend.Application/Games/GameAppService.cs
@@ -92,7 +92,7 @@ namespace web_backend.Games
 
                 var game = new Game
                 {
-                    GameUrl = input.GameUrl,
+                    GameUrl = "",
                     HomeTeam = input.HomeTeam,
                     AwayTeam = input.AwayTeam,
                     HomeScore = input.HomeScore,
